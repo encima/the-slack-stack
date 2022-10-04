fac7da4df5d579d819300d643c6bd9babfd70f38
Chris Gwilliams <chrisg@aiven.io>
2022-10-04 11:32:16 +0300

    add flutter app as submodule and fix diagram generation, presentation for exercises included

---
+++ presentation/slack_stack_steps.md
@@ -0,0 +1,30 @@
+# The Slack Stack - Implementation
+
+<br/><br/><br/>
+<br/>
+
+<br/>
+
+### Chris Gwilliams
+
+#### Solutions Architect and OSS Enthusiast - [Aiven.io](https://aiven.io)
+
+[chrisg@aiven.io](mailto:chrisg@aiven.io) / [github.com/encima](https://github.com/encima/) / [chris.pub](https://chris.pub)
+
+![Aiven](slack_stack.assets/generic-hero-3.png)
+
+
+---
+
+## Step 1 - Let's make a chat app!
+
+We want to start with a simple chat app and that needs a frontend, so let's start there! This app can be found in the `code` folder, under `app`.
+
+
+## Step 2 - Make it work!
+
+We have a website, that's cool. But it does nothing. So let's make it do something! We need to be able to store messages and users and things, how could we do that?
+
+## Step 3 - PostgreSQL!!
+
+The mother, father and lord of all databases: PostgreSQL. As traditional as your grandmother but much more stable.
--- scripts/generate_diagrams.py
+++ scripts/generate_diagrams.py
@@ -5,16 +5,16 @@ from diagrams.onprem.analytics import Spark
 from diagrams.onprem.compute import Server
 from diagrams.onprem.database import PostgreSQL, Cassandra, InfluxDB
 from diagrams.onprem.inmemory import Redis
-from diagrams.onprem.logging import Fluentd
+from diagrams.onprem.aggregator import Fluentd
 from diagrams.onprem.monitoring import Grafana, Prometheus
 from diagrams.onprem.network import Nginx
 from diagrams.onprem.queue import Kafka
+from diagrams.alibabacloud.analytics import OpenSearch
 
-from diagrams.programming.framework import React
-from diagrams.elastic.elasticsearch import Elasticsearch
+from diagrams.programming.framework import Flutter
 
 
-outdir="./slack_stack.assets/"
+outdir="../presentation/slack_stack.assets/"
 files = ["{}{}".format(outdir, f'step_{x}') for x in range(1, 6)]
 steps = ["Step 1 - Webapp and Database", "Step 2 - Scaling Messages", "Step 3 - Searching Messages", "Step 4 -Client-side Caching", "Step 5 - Logging and Metrics"]
 
@@ -26,76 +26,68 @@ with Diagram(show=False) as d:
     # Step 1    
     with d, Diagram(show=True, filename=files[0]):
         with Cluster("Web"):
-            web_clients = [React("Client 1"), React("Client 2")]
-        with Cluster("API and Database"):
-            with Cluster("Heroku"):
-                hasura = Server("GraphQL")
+            web_clients = [Flutter("Client 1"), Flutter("Client 2")]
+        with Cluster("Database"):
             with Cluster("Aiven"):
                 pg = PostgreSQL("DB")
-        web_clients << Edge(color="green") >> hasura >> Edge(color="green") << pg
+        web_clients << Edge(color="green") << pg
     
     # Step 2
     with Diagram(show=True, filename=files[1]):
         with Cluster("Web"):
-            web_clients = [React("Client 1"), React("Client 2")]
-        with Cluster("API and Database"):
-            with Cluster("Heroku"):
-                hasura = Server("GraphQL")
+            web_clients = [Flutter("Client 1"), Flutter("Client 2")]
+        with Cluster("Database"):
             with Cluster("Aiven"):
                 pg = PostgreSQL("DB")
-        web_clients << Edge(color="green") >> hasura >> Edge(color="green") << pg
+        web_clients << Edge(color="green") << pg
         
         with Cluster("Aiven"):
-            kfk = Kafka("Kakfa")
+            kfk = Kafka("Kafka")
         web_clients << Edge(color="red", label="Produce/Consume") >> kfk
         kfk >> Edge(color="red", label="Postgres Sink Connector") >> pg
      
     # Step 3   
     with Diagram(show=True, filename=files[2]):
         with Cluster("Web"):
-            web_clients = [React("Client 1"), React("Client 2")]
-        with Cluster("API and Database"):
-            with Cluster("Heroku"):
-                hasura = Server("GraphQL")
+            web_clients = [Flutter("Client 1"), Flutter("Client 2")]
+        with Cluster("Database"):
             with Cluster("Aiven"):
                 pg = PostgreSQL("DB")
-        web_clients << Edge(color="green") >> hasura >> Edge(color="green") << pg
+        web_clients << Edge(color="green")  << pg
         
         with Cluster("Aiven"):
-            kfk = Kafka("Kakfa")
+            kfk = Kafka("Kafka")
         web_clients << Edge(color="red", label="Produce/Consume") >> kfk
         kfk >> Edge(color="red", label="Postgres Sink Connector") >> pg
         
         with Cluster("Message Search"):
-            es = Elasticsearch("Elasticsearch")
-        kfk >> Edge(color="blue", label="Elasticsearch Sink Connector") >> es
+            es = OpenSearch("OpenSearch")
+        kfk >> Edge(color="blue", label="OpenSearch Sink Connector") >> es
         es << Edge(color="blue",label="Search") >> web_clients
       
     # Step 4  
     with Diagram(show=True, filename=files[3]):
         with Cluster("Web"):
-            web_clients = [React("Client 1"), React("Client 2")]
-        with Cluster("API and Database"):
-            with Cluster("Heroku"):
-                hasura = Server("GraphQL")
+            web_clients = [Flutter("Client 1"), Flutter("Client 2")]
+        with Cluster("Database"):
             with Cluster("Aiven"):
                 pg = PostgreSQL("DB")
-        web_clients << Edge(color="green") >> hasura >> Edge(color="green") << pg
+        web_clients << Edge(color="green")  << pg
         
         with Cluster("Aiven"):
-            kfk = Kafka("Kakfa")
+            kfk = Kafka("Kafka")
         web_clients << Edge(color="red", label="Produce/Consume") >> kfk
         kfk >> Edge(color="red", label="Postgres Sink Connector") >> pg
         
         with Cluster("Message Search"):
-            es = Elasticsearch("Elasticsearch")
-        kfk >> Edge(color="blue", label="Elasticsearch Sink Connector") >> es
+            es = OpenSearch("OpenSearch")
+        kfk >> Edge(color="blue", label="OpenSearch Sink Connector") >> es
         es << Edge(color="blue",label="Search") >> web_clients
         
         with Cluster("Caching"):
             with Cluster("Aiven"):
                 rds = Redis("Redis")
-        web_clients << Edge(color="yellow", label="Response") << rds << Edge(color="yellow", label="Response") << hasura
+        web_clients << Edge(color="yellow", label="Response") << rds
         
     # Step 5
     with Diagram(show=True, filename=files[4]):
@@ -104,36 +96,32 @@ with Diagram(show=False) as d:
             graf = Grafana("Dashboards")
 
         with Cluster("Web"):
-            web_clients = [React("Client 1"), React("Client 2")]
-        with Cluster("API and Database"):
-            with Cluster("Heroku"):
-                hasura = Server("GraphQL")
+            web_clients = [Flutter("Client 1"), Flutter("Client 2")]
+        with Cluster("Database"):
             with Cluster("Aiven"):
                 with Cluster("Database HA"):
                     pg = PostgreSQL("DB")
                     pg - Edge(color="brown", style="dotted") - PostgreSQL("secondary") << Edge(label="collect") << metrics
-                    web_clients << Edge(color="green") << hasura
-                    hasura >> Edge(color="green") << pg
-        web_clients << Edge(color="green") >> hasura >> Edge(color="green") << pg
+                    web_clients << Edge(color="green") << pg
+        web_clients << Edge(color="green")  << pg
         
         with Cluster("Aiven"):
-            kfk = Kafka("Kakfa")
+            kfk = Kafka("Kafka")
         web_clients << Edge(color="red", label="Produce/Consume") >> kfk
         kfk >> Edge(color="red", label="Postgres Sink Connector") >> pg
         
         with Cluster("Message Search"):
-            es = Elasticsearch("Elasticsearch")
-        kfk >> Edge(color="blue", label="Elasticsearch Sink Connector") >> es
+            es = OpenSearch("OpenSearch")
+        kfk >> Edge(color="blue", label="OpenSearch Sink Connector") >> es
         es << Edge(color="blue",label="Search") >> web_clients
         
         with Cluster("Caching"):
             with Cluster("Aiven"):
                 rds = Redis("Redis")
                 rds - Edge(color="brown", style="dashed") - Redis("replica") << Edge(label="collect") << metrics
-        web_clients << Edge(color="yellow", label="Response") << rds << Edge(color="yellow", label="Response") << hasura    
+        web_clients << Edge(color="yellow", label="Response") << rds 
         
         kfk >> Edge(color="purple", style="dotted", label="collect") >> metrics
-        hasura >> Edge(color="purple",  style="dotted", label="collect") >> metrics
         pg >> Edge(color="purple",  style="dotted", label="collect") >> metrics
         
         metrics >> Edge(style="dotted", label="visualise") >> graf
